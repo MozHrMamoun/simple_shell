2e50e600cd1ecf778a864166ce329e8ce55f96a5
MozHrMamoun <gopo-chic10@hotmail.com>
2023-11-10 10:09:48 -0800

    shell file

--- shell.h
+++ shell.h
@@ -15,37 +15,37 @@
 /* All of strings functions*/
 
 /* Functions_1-4.c */
-int compstr (char *s1, char *s2);
-char *dupstr(const char *s);
-char *concat(char *s1, char *s2, int b);
-char **stringtoword(char *str, char d);
+int compstr(char *, char *);
+char *dupstr(const char *);
+char *concat((char *, char *, int);
+char *strtow2(char *, char);
 
 /* Functions_5-8.c */
-int lenstr(char *s);
+int lenstr(char *);
 char *copystr(char *dest, char *src);
-char *cpystr(char *dest, char *src, int c);
-char **strtoword(char *str, char *d);
+char *cpystr(char *, char *, int);
+char **strtow(char *, char *);
 
 /* Functions_9-11.c */
-char *beginwith(const char *haystack, const char *needle);
-void _puts(char *strn);
-char *findchar(char *s, char c);
+char *beginwith(const char *, const char *);
+void _puts(char *);
+char *findchar(char *, char);
 
 /* Functions_12-14.c */
-char *catstr(char *dest, char *src)
-int _putchar(char c)
+char *catstr(char *, char *);
+int _putchar(char);
 
 /* end string functios*/
 
 /* All of memory functios*/
 
 /* memo_functions_1-2.c*/
-void strfree(char **ss);
-int panfree(void **p);
+void strfree(char **);
+int panfree(void **);
 
 /*memo_functions_3-4.c*/
-char *setmemo(char *p, char b, unsigned int c);
-void *realloct(void *poi, unsigned int old_size, unsigned int new_size);
+char *setmemo(void *, unsigned int, unsigned int);
+void *realloct(void *, unsigned int, unsigned int);
 
 /* end of memory functios*/
 
@@ -53,60 +53,60 @@ void *realloct(void *poi, unsigned int old_size, unsigned int new_size);
 
 /* extra_functions_1-5.c*/
 int strtoint(char *s);
-int is_delim(char c, char *delm);
-int print_deci(int input, int f);
-int stoi(char *s);
-void rmcomments(char *add);
+int is_delim(char, char *);
+int print_deci(int, int);
+int stoi(char *);
+void rmcomments(char *);
 
 /* extra_functions_6-9.c*/
-int isinteractive(info_t *statement);
-void printerror(info_t *info, char *errstr);
-int isalphab(int c);
-char *convenumber(long int num, int base, int arg);
+int isinteractive(info_t *);
+void printerror(info_t *, char *);
+int isalphab(int);
+char *convenumber(long int, int, int);
 
 /* end of extra functios */
 
 /* All of list functions*/
 
 /*Stringlist_1-5.c*/
-size_t listlen(const list_t *head);
-list_t *addnodeatend(list_t **head, const char *str, int c);
-size_t print_list(const list_t *head);
-int deletenodeatindex(list_t **head, unsigned int index);
-ssize_t get_node_index(list_t *head, list_t *node);
+size_t listlen(const list_t *)
+list_t *addnodeatend(list_t **, const char *, int);
+size_t print_list(const list_t *);
+int deletenodeatindex(list_t **, unsigned int);
+ssize_t getnodeindex(list_t *, list_t *);
 
 /*Stringlist_6-10.c*/
-list_t *addnode(list_t **head, const char *str, int num);
-char **list_to_strings(list_t *head);
-size_t printliststr(const list_t *head);
-list_t *nodestartswith(list_t *node, char *prefix, char c);
-void listfree(list_t **head_ptr);
+list_t *addnode(list_t **, const char *, int);
+char **list_to_strings(list_t *);
+size_t printliststr(const list_t *);
+list_t *nodestartswith(list_t *, char *, char);
+void listfree(list_t **);
 
 /* end of list functions*/
 
 /* All of error string functions*/
 
 /*error_string_functions.c*/
-int erroputchar(char c);
-int prinput(char *str, int f);
-void erroputs(char *str);
-int prif(char c, int f);
+int erroputchar(char);
+int prinput(char *str, int fd);
+void erroputs(char *)
+int prif(char c, int fd);
 
 /* end of error string functions*/
 
 /* all of enviroments functions*/
 
 /*enviro_1-4.c*/
-int myenvro(info_t *info);
-int rmenvro(info_t *info, char *c);
-int mysetenvro(info_t *info);
-int makeenvlist(info_t *info);
+int myenvro(info_t *);
+int rmenvro(info_t *, char *);
+int mysetenvro(info_t *);
+int makeenvlist(info_t *);
 
 /*enviro_5-9*/
-char **getenvirocopy(info_t *info);
-char *getenvro(info_t *info, const char *name);
-int setenvro(info_t *info, char *var, char *value);
-int unsetenvro(info_t *info);
+char **getenvirocopy(info_t *);
+char *getenvro(info_t *, const char *);
+int setenvro(info_t *, char *, char *);
+int unsetenvro(info_t *);
 
 /* end of enviroments functions*/
 
@@ -115,7 +115,7 @@ int unsetenvro(info_t *info);
 /*io_file_functions.c*/
 int writehistory(info_t *info);
 int addhistorylist(info_t *info, char *buff, int linecount);
-char *gethistoryfile(info_t *info);
+char *(info_t *info);
 int readhistory(info_t *info);
 int renumberhistory(info_t *info);
 
@@ -124,18 +124,18 @@ int renumberhistory(info_t *info);
 /* all of chain functions*/
 
 /*chain.c*/
-int replacestring(char **old, char *new);
-int replacealias(info_t *info);
-void checkchain(info_t *info, char *buff, size_t *p, size_t i, size_t leng);
-int replacevars(info_t *info);
-int ischain(info_t *info, char *buff, size_t *p);
+int replacestring(char **, char *);
+int replacealias(info_t *);
+void checkchain(info_t *, char *, size_t *, size_t, size_t);
+int replacevars(info_t *);
+int ischain(info_t *, char *, size_t *);
 
 /* all of path functions*/
 
 /*path.c*/
-int iscmd(info_t *info, char *pat);
-char *findpath(info_t *info, char *patstr, char *cmd);
-char *duplichars(char *patstr, int start, int stop);
+int iscmd(info_t *, char *);
+char *findpath(info_t *, char *, char *);
+char *duplichars(char *, int, int);
 
 /* end of path functions*/
 
@@ -143,46 +143,44 @@ char *duplichars(char *patstr, int start, int stop);
 
 /*getlines.c*/
 
-void sigintHandl(__attribute__((unused))int sig_num);
-ssize_t readbuff(info_t *info, char *buff, size_t *i);
-ssize_t inputbuff(info_t *info, char **buff, size_t *leng);
-int getnextline(info_t *info, char **ptr, size_t *leng);
-ssize_t getinput(info_t *info);
+void sigintHandl(int);
+int getnextline(info_t *, char **, size_t *);
+ssize_t getinput(info_t *);
 
 /* end of getlines functions*/
 
 /* all of info functions*/
 
 /*info.c*/
-void clearinfo(info_t *info);
-void setinfo(info_t *info, char **av);
-void freeinfo(info_t *info, int all);
+void clearinfo(info_t *);
+void setinfo(info_t *, char **);
+void freeinfo(info_t *, int);
 
 /* end of info functions*/
 
 /* all of hs functions*/
 
 /*hs.c*/
-int hs(info_t *info, char **av);
-void forkcmd(info_t *info);
-void findcmd(info_t *info);
-int findbuilt(info_t *info);
+int hs(info_t *info, char **);
+void forkcmd(info_t *;
+void findcmd(info_t *);
+int findbuilt(info_t *);
 
 /* end of hs functions*/
 
 /* all of builtin functions*/
 
 /*bultin.c*/
-int myexit(info_t *info);
-int myhistory(info_t *info);
-int setalias(info_t *info, char *strg);
-int myalias(info_t *info);
-int myhelp(info_t *info);
-int mycd(info_t *info);
-int printalias(list_t *node);
-int unsetalias(info_t *info, char *strg);
+int myexit(info_t *);
+int myhistory(info_t *);
+int myalias_myalias(info_t *);
+int myhelp(info_t *);
+int mycd(info_t *);
 
 /* end of builtin functions*/
+
+/* loophsh.c */
+int loophsh(char **);
 
 /* for command chaining */
 #define CMD_NORM	0
